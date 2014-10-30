6c8e7447850800ab0558631c45eafcdb3749f57e
Jordi <jordi1704@outlook.com>
2014-10-31 00:00:21 +0100

    Implementation of Color & Light sensor methods

--- src/Color.cpp
+++ src/Color.cpp
@@ -12,26 +12,64 @@ Color::Color (Port_t Port, ColorMode_t Mode, DataLogger* Logger) :
 							Sensor(Port,Logger)
 {
   m_DeviceID=" COLOR:"+sPortName[Port];
-  SetSensorMode(sColorMode[Mode]);
+  m_ColorMode=Mode;
+  SetColorMode(Mode);
 }
 
 Color::~Color ()
 {
-  // TODO Auto-generated destructor stub
+}
+
+void Color::SetColorMode(ColorMode_t Mode)
+{
+  Sensor::SetSensorMode(sColorMode[Mode]);
 }
 
 LightSensor::LightSensor (Port_t Port, DataLogger* Logger) :
     Color(Port,REFLECTED,Logger)
 {
+  m_DeviceID=" LIGHT_SENSOR:"+sPortName[Port];
 }
 
 LightSensor::~LightSensor()
 {
 }
 
-
 int LightSensor::GetReflected()
 {
+  if(m_ColorMode!=REFLECTED) SetColorMode(REFLECTED);
   return atoi(GetSensorValue(sColorValue[REFLECTED]).c_str());
+}
+
+int LightSensor::GetAmbient()
+{
+  if(m_ColorMode!=AMBIENT) SetColorMode(AMBIENT);
+  return atoi(GetSensorValue(sColorValue[AMBIENT]).c_str());
+}
+
+ColorSensor::ColorSensor(Port_t Port, DataLogger* Logger) :
+    Color(Port,COLOR,Logger)
+{
+  m_DeviceID=" COLOR_SENSOR:"+sPortName[Port];
+}
+
+ColorSensor::~ColorSensor()
+{
+}
+
+Color_t ColorSensor::GetColor()
+{
+  string color;
+  if(m_ColorMode!=COLOR) SetColorMode(COLOR);
+  color=Sensor::GetSensorValue(sColorValue[COLOR]);
+  return (color==sColor[NONE]) ? NONE :
+	  (color==sColor[BLACK]) ? BLACK :
+	   (color==sColor[BLUE]) ? BLUE :
+	    (color==sColor[GREEN]) ? GREEN :
+             (color==sColor[YELLOW]) ? YELLOW :
+              (color==sColor[RED]) ? RED :
+               (color==sColor[WHITE]) ? WHITE :
+        	(color==sColor[BROWN]) ? BROWN : NONE;
+
 }
 
--- src/Color.h
+++ src/Color.h
@@ -10,15 +10,15 @@
 
 #include "Sensor.h"
 
-enum ColorMode_t {REFLECTED, AMBIENT, COLOR, _COLOR_MODES_};
-const string sColorMode[_COLOR_MODES_+1]={"COL-REFLECT","COL-AMBIENT",
+enum ColorMode_t {REFLECTED, AMBIENT, COLOR, COLOR_MODES};
+const string sColorMode[COLOR_MODES+1]={"COL-REFLECT","COL-AMBIENT",
                                           "COL-COLOR","mode"};
 
 enum Color_t {NONE, BLACK, BLUE, GREEN, YELLOW, RED, WHITE, BROWN, COLORS};
 const string sColor[COLORS]={"none", "black", "blue", "green",
 			     "yellow", "red", "white","brown"};
 
-const string sColorValue[_COLOR_MODES_]={"value0","value0","value0"};
+const string sColorValue[COLOR_MODES]={"value0","value0","value0"};
 
 class Color : public Sensor
 {
@@ -26,15 +26,27 @@ public:
   Color (Port_t Port, ColorMode_t Mode, DataLogger* Logger=NULL);
   virtual
   ~Color ();
+  void SetColorMode(ColorMode_t Mode);
+  ColorMode_t m_ColorMode;
 };
 
 class LightSensor : private Color
 {
 public:
   LightSensor(Port_t Port, DataLogger* Logger=NULL);
-  int GetReflected();
   virtual
-  ~LightSensor();
+    ~LightSensor();
+  int GetReflected();
+  int GetAmbient();
+};
+
+class ColorSensor : private Color
+{
+public:
+  ColorSensor(Port_t Port, DataLogger* Logger=NULL);
+  virtual
+  ~ColorSensor();
+  Color_t GetColor();
 };
 
 
