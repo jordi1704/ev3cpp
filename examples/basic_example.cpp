b3c231e528e14c07911327b6f8f4319b72756364
Jordi <jordi1704@outlook.com>
2014-10-31 20:30:38 +0100

    Basic example fixed

--- examples/basic_example.cpp
+++ examples/basic_example.cpp
@@ -3,9 +3,6 @@
 #include "Touch.h"
 #include "Color.h"
 
-#include "globalDefs.h" //TODO remove after ONE_... not needed
-
-#include <iostream> //TODO remove after cout
 
 int main()
 {
@@ -13,49 +10,23 @@ int main()
   DataLogger* myLogger=new DataLogger("ev3.log",DBG_LVL_0);
 
   // Create a tacho motor connected on output port A and get traces
-  //Tacho* myMotor=new Tacho(OUT_A,myLogger);
+  Tacho* myMotor=new Tacho(OUT_A,myLogger);
+
+  // Create a light sensor on input port 3 and get traces
+  LightSensor* myLightSensor=new LightSensor(IN_3,myLogger);
 
   // Create a touch sensor on input port 4. As no logger pointer is specified,
   // there will be no logged info about touch sensor
   Touch* myTouch=new Touch(IN_4);
 
-  LightSensor* myReflected=new LightSensor(IN_3,myLogger);
-
   // Start motor at power 50%
-  //myMotor->RunForever(50);
+  myMotor->RunForever(50);
 
-  // Wait until touch sensor is pressed to stop the motor
-  while(!myTouch->IsPressed()){
-      cout << "Reflected value is: " << myReflected->GetReflected() << endl;
-      usleep(ONE_SECOND);
-      cout << "Ambient value is: " << myReflected->GetAmbient() << endl;
-      usleep(ONE_SECOND);
-  }
-
-  cout << "Changing mode" << endl;
-
-  myReflected->~LightSensor();
-
-  Colorimeter* myColorimeter=new Colorimeter(IN_3,myLogger);
-  string color;
-  Color_t colordetected;
-  usleep(5*ONE_SECOND);
-
-  while(!myTouch->IsPressed()){
-      colordetected=myColorimeter->GetColor();
-      color=sColor[colordetected];
-      cout << color <<endl;
-      usleep(ONE_SECOND);
-  }
-
-
-
-
-
-  //TODO complete example
+  // Wait until touch sensor is pressed or room light is off to stop the motor
+  while(!myTouch->IsPressed() && (myLightSensor->GetAmbient()!=0));
 
   // Stop the motor
-  //myMotor->Stop();
+  myMotor->Stop();
 
   return(0);
 }
